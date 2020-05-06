38a3c32b93bb7b43b4dc325cbe6eb0d424592eb2
Shahzaib Saleem <shahzaib.saleem1997@gmai.com>
2020-05-06 17:42:12 +0500

    admin panel users page connected with backend

--- frontend/src/Services/httpService.js
+++ frontend/src/Services/httpService.js
@@ -3,11 +3,14 @@ import axios from 'axios';
 import {
     toast
 } from 'react-toastify';
-import auth from './authService';
+import {
+    login,
+    getJwt
+} from './authService';
 
 
 
-
+axios.defaults.headers.common['x-auth-token'] = getJwt();
 axios.interceptors.response.use(null, error => {
     const expectedError =
         error.response &&
